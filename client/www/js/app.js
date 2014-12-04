0c3dd69cb832aee7ce4b9cc1bf09befd447d09c8
asakurasol <mamamia1734@gmail.com>
2014-12-04 14:09:50 -0800

    add basic local storage to prevent additional api calls

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -5,7 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ionic.contrib.ui.tinderCards'])
+angular.module('starter', ['ionic',
+   'starter.controllers',
+   'starter.services',
+   'ionic.contrib.ui.tinderCards'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
