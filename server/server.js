4cdac3857feb5bab9985a2aaf67c134452b07d5c
Anastasia Zotova <anastasiazotova@MacBook-Pro.local>
2014-12-04 12:12:42 -0800

    Add cors to treat cross-origin requests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 // Middleware
 var morgan = require('morgan');
 var parser = require('body-parser');
+var cors = require('cors');
 
 // Router
 var router = require('./routes.js');
@@ -12,6 +13,9 @@ module.exports.app = app;
 
 // Set what we are listening on.
 app.set("port", 3000);
+
+//Taking care of cross-origin requests
+app.use(cors());
 
 // Logging and parsing
 app.use(morgan('dev'));
