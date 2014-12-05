299369c29f326d3e287354347bcae95fb673b2e4
asakurasol <mamamia1734@gmail.com>
2014-12-05 12:19:49 -0800

    add card on destroy instead of swipe left/right

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -16,19 +16,19 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('DashCtrl', function($scope, $http, $localstorage,TDCardDelegate) {
+.controller('DashCtrl', function($scope, $http, $localstorage,TDCardDelegate,$timeout) {
   var cardTypes;
 
   $scope.cardDestroyed = function(index) {
     $scope.cards.splice(index, 1);
-    // $scope.addCard();
     console.log('destroyed');
+    $timeout($scope.addCard,0);
   };
 
   $scope.addCard = function() {
 
     var newCard = cardTypes.splice(Math.floor(Math.random() * cardTypes.length),1)[0];
-    //newCard.id = Math.random();
+    newCard.id = Math.random();
     console.log('new card is', newCard);
     $scope.cards.push(angular.extend({}, newCard));
   }
@@ -41,7 +41,7 @@ angular.module('starter.controllers', [])
       $http.get("https://mealmatch2.herokuapp.com/test/explore").success(function(data, status, headers, config) {
         console.log('success', data);
         cardTypes = data;
-        $scope.cards = Array.prototype.slice.call(cardTypes, 1,4);
+        $scope.cards = Array.prototype.slice.call(cardTypes, 1,1);
         $localstorage.setObject('temp', data);
       })
       .error(function(data, status, headers, config) {
@@ -52,7 +52,7 @@ angular.module('starter.controllers', [])
       //make a copy of local storage so that local storage remains persistant
       cardTypes = Array.prototype.slice.call($localstorage.getObject('temp'));
       //load 3 cards for view
-      $scope.cards = Array.prototype.splice.call(cardTypes, 1,4);
+      $scope.cards = Array.prototype.splice.call(cardTypes, 1,1);
     }
   };
 
@@ -62,11 +62,11 @@ angular.module('starter.controllers', [])
 .controller('CardCtrl', function($scope, TDCardDelegate) {
   $scope.cardSwipedLeft = function(index) {
     console.log('LEFT SWIPE');
-    $scope.addCard();
+    // $scope.addCard();
   };
   $scope.cardSwipedRight = function(index) {
     console.log('RIGHT SWIPE');
-    $scope.addCard();
+    // $scope.addCard();
   };
 })
 
