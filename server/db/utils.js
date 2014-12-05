49dc5a800f9a5a7d503db197c35dd0c153082a2c
Arfurboy <arfurboy@gmail.com>
2014-12-05 14:55:48 -0800

    initialize database helper functions file in utils.js

---
+++ server/db/utils.js
@@ -0,0 +1,22 @@
+var db = require('./db');
+//**********************************************
+// General Database Helpers
+//**********************************************
+var addListOfRecipes = function(){}
+
+var findUser = function(userId, password, callback){
+
+}
+
+var addUser = function(userId, password, callback){
+  
+}
+
+//**********************************************
+// User favorites list helpers
+//**********************************************
+var getUserFavorites = function(){}
+
+var addRecipeToUserFavorites = function(){}
+
+var removeRecipeFromUserFavorites = function(){}
