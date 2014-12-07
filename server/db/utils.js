5b0b95cdf9aa1ea657d65de5602994fec1f7cd49
Arfurboy <arfurboy@gmail.com>
2014-12-07 15:46:41 -0800

    Implement more database helper functions and remove test.js file

--- server/db/utils.js
+++ server/db/utils.js
@@ -43,6 +43,9 @@ var addListOfRecipes = function(arrayOfRecipes, callback){
   recurse(arrayOfRecipes.length - 1);
 }
 
+// Input: callback
+// Output: invoke callback(err, recipeList)
+// recipeList will be an array of recipe objects
 var getAllRecipes = function(callback){
     var results =[];
     db.Recipe.findAll({include: [db.Ingredient]}).complete(function(err,result){
@@ -59,39 +62,113 @@ var getAllRecipes = function(callback){
             }
             results.push(temp);
         }
-        callback(results);
+        callback(err, results);
     });
 }
 
-// If user exists: returns user object
-// if user does not exist: returns null
+// Input: username, password, and REQUIRED callback 
+// Output: invoke callback(err, user)
+// user will be an object with user data
 var findUser = function(username, password, callback){
   db.User.find({where: {username: username, password: password}})
   .complete(function(err, result){
-    callback(err, result.dataValues);
+    if (result === null) {
+      callback(err, result);
+    } else {
+      callback(err, result.dataValues);
+    }
   });
 }
 
+// Input: username, password, and optional callback
+// Ouput: If callback provided, invoke callback(err, user)
+// user will be an object with user data
 var addUser = function(username, password, callback){
   db.User.create({
     username: username,
     password: password
-  }).complete(function(err, result){
-    callback(err, result.dataValues);
+  }).complete(function(err, user){
+    db.Favorite.create().complete(function(err, fav){
+      fav.setUser(user).complete(function(err, results){
+        if(callback){
+          callback(err, user.dataValues);
+        }
+      });
+    });
   });
 }
 
 //**********************************************
 // User favorites list helpers
 //**********************************************
-var getUserFavorites = function(){}
 
-var addRecipeToUserFavorites = function(){}
+// Input: user object, and a required callback
+// Output: invokes callback(err, results)
+// results will be an array of recipe objects
+var getUserFavorites = function(user, callback){
+  db.Favorite.find({where: {UserId: user.id}})
+  .complete(function(err,fav){
+    console.log(fav);
+    fav.getRecipes().complete(function(err,recipes){
+      // Format results
+      if (recipes) {
+        var results = [];
+        var start = recipes.length-1;
+        var recurse = function(index){
+          results[index] = recipes[index].dataValues;
+          results[index].ingredients = [];
+          recipes[index].getIngredients().complete(function(err, ingreds){
+            for(var i=0; i<ingreds.length; i++){
+              results[index].ingredients.push(ingreds[i].dataValues.name);
+            }
+            if(index > 0) recurse(index-1);
+            else callback(err, results);
+          });
+        }
+        if (start > 0) recurse(start);
+        else callback(err, recipes);
+      } else {
+        callback(err, recipes);
+      }
+    });
+  });
+}
 
-var removeRecipeFromUserFavorites = function(){}
+// Input: user object , recipe object, and an optional callback
+// Output: if provided a callback, invoke callback(err, results)
+var addRecipeToUserFavorites = function(user, recipe, callback){
+  db.Favorite.find({where: {UserId: user.id}})
+  .complete(function(err,fav){
+    db.Recipe.find({where: {id: recipe.id}})
+    .complete(function(err,recipeEntry){
+      fav.addRecipes(recipeEntry).complete(function(err,results){
+        if(callback){
+          callback(err, results);
+        }
+      });
+    });
+  }); 
+}
 
+// Input: user object, recipe object, and an optional callback
+// Output: if provided a callback, invoke callback(err, removed)
+// if successful, removed === [1]
+var removeRecipeFromUserFavorites = function(user, recipe, callback){
+ db.Favorite.find({where: {UserId: user.id}})
+ .complete(function(err,fav){
+   db.Recipe.find({where: {id: recipe.id}})
+   .complete(function(err,recipeEntry){
+     fav.removeRecipe(recipeEntry).complete(function(err, removed){
+       if(callback){
+         callback(err, removed);
+       }
+     });
+   });
+ }); 
+}
 
 exports.addListOfRecipes = addListOfRecipes;
+exports.getAllRecipes = getAllRecipes;
 exports.findUser = findUser;
 exports.addUser = addUser;
 exports.getUserFavorites = getUserFavorites;
