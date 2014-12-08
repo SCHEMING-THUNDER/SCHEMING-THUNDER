2b2c94bfa0f70bbbfbe0ddc5a32810926cc54559
asakurasol <mamamia1734@gmail.com>
2014-12-08 11:34:23 -0800

    added basic d3 horizontal bar graph

--- client/www/js/recipeDetailController.js
+++ client/www/js/recipeDetailController.js
@@ -5,3 +5,26 @@ angular.module('starter.controllers.recipe_detail', [])
   $scope.recipe = $scope.getData()[$stateParams.friendId];
   console.log($scope.recipe);
 })
+.directive('bars', function ($parse) {
+      return {
+         restrict: 'E',
+         replace: true,
+         template: '<div id="chart"></div>',
+         link: function (scope, element, attrs) {
+           var flavors = JSON.parse(attrs.data);
+           var data = [];
+           for(var key in flavors){
+           	data.push(Math.floor(flavors[key]*100));
+           }
+           console.log(data);
+           chart = d3.select('#chart')
+             .append("div").attr("class", "chart")
+             .selectAll('div')
+             .data(data).enter()
+             .append("div")
+             .transition().ease("elastic")
+             .style("width", function(d) { return d + "%"; })
+             .text(function(d) { return d + "%"; });
+         } 
+      };
+   });
